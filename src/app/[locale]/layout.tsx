92b793a632c169eccb620707c101827175ad0503
Adoniev Ivan <adonjew.vaniya@gmail.com>
2023-09-01 15:24:03 +0300

    Added vercel analytics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -9,6 +9,7 @@ import {Root} from "./Root"
 import favicon from "@public/assets/svg/favicon.svg"
 import {headers} from "next/headers";
 import {detectIsMobile} from "@/helpers/detectIsMobile";
+import {Analytics} from "@vercel/analytics/dist/react";
 
 
 
@@ -42,6 +43,7 @@ export default async function RootLayout({children,  params: {locale}}: {
               <Root>
                   {children}
               </Root>
+              <Analytics/>
           </NextIntlClientProvider>
       </body>
       </html>
