43b98772455fa061491ea2bb6636e6fc69ed9bbb
Adoniev Ivan <adonjew.vaniya@gmail.com>
2023-11-12 19:19:32 +0200

    Improved public | private routes

--- src/middlewares/withPrivateRoute.ts
+++ src/middlewares/withPrivateRoute.ts
@@ -1,20 +1,21 @@
 import {MiddlewareFactory} from "@/middlewares/types";
 import {NextFetchEvent, NextMiddleware, NextRequest, NextResponse} from "next/server";
 import {Routes} from "@/router";
-import axios from "axios";
 import {BASE_URL} from "@/api";
 
 const config = {
     matcher: '/((?!api|_next|.*\\..*).*)'
 };
-const privateRoutes = [Routes.home];
+const privateRoute = [Routes.home];
+const publicRoutes = [Routes.default, Routes.register, Routes.login];
 const withPrivateRoute:MiddlewareFactory = (next:NextMiddleware)=>{
     return async(request:NextRequest, _next:NextFetchEvent)=>{
         if(!request.nextUrl.pathname.match(config.matcher)
             || request.nextUrl.pathname.includes('_next')
             || request.nextUrl.pathname.includes('_api')) return next(request, _next);
-
-        if(!privateRoutes.some(route => request.nextUrl.pathname.includes(route)))  return next(request, _next);
+        const isPrivateRoute = privateRoute.some(route => request.nextUrl.pathname.includes(route));
+        const isPublicRoute = publicRoutes.some(route => request.nextUrl.pathname.includes(route))
+        if(!isPublicRoute || !isPublicRoute)  return next(request, _next);
 
         const token = request.cookies.get("token");
         if(!token)  return NextResponse.redirect(new URL(Routes.default, request.url));
@@ -26,8 +27,14 @@ const withPrivateRoute:MiddlewareFactory = (next:NextMiddleware)=>{
             body:JSON.stringify({token:token.value})
         })
         const json = await response.json();
-        if(!json) return NextResponse.redirect(new URL(Routes.default, request.url));
-        if(!json.data.verified) return NextResponse.redirect(new URL(Routes.default, request.url));
+        if(isPrivateRoute){
+            if(!json) return NextResponse.redirect(new URL(Routes.default, request.url));
+            if(!json.data.verified) return NextResponse.redirect(new URL(Routes.default, request.url));
+            return next(request, _next);
+        }
+        if(isPublicRoute && process.env.NODE_ENV === "production"){
+            return NextResponse.redirect(new URL(Routes.home, request.url));
+        }
 
         return next(request, _next);
     }
